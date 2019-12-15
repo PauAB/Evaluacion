f938c6e546f6e6351c7d91c433bc4e5a0f035372
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2019-12-15 18:01:51 +0100

    First commit

---
+++ Evaluacion/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject inventory;
+    private GameObject[] slots;
+
+    private bool inventoryEnabled;
+    private int allSlots;    
+    private int enabledSlots;
+    private bool slotEnabled;
+}
